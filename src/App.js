ee121517d7e3a43119060ffcdb4ad6940ecbb253
Yash <yashpaota@gmail.com>
2023-03-02 03:19:02 +0530

    Login Page commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import profile from "./Images/profile.png";
+import email from "./Images/email.jpg";
+import pass from "./Images/pass.png";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-img">
+              <img src={profile} alt="profile-pic" className="profile"/>
+            </div>
+          </div>
+          <div>
+            <h1>Login Page</h1>
+            <div >
+              <img src={email} alt="email-pic" className="icon-img" />
+              <input type="text" placeholder="user name" className="name" />
+            </div>
+            <div className="secondInput-container">
+              <img src={pass} alt="pass-pic" className="icon-img" />
+              <input type="password" placeholder="password" className="name" />
+            </div>
+            <div className="button-container">
+            <button>Login</button>
+            </div>
+            <p className="link">
+              <a href="#">Forgot Password</a> or <a href="#">Sign Up</a>
+            </p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
